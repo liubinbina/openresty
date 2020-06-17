1b7d81c8d9740747c8c8e0c1f27401e566c4fa57
nash <nash@iffy.me>
2020-06-17 12:10:00 +0800

    ...

--- inotify.sh
+++ inotify.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/with-contenv bash
 echo >&2 "starting watcher"
 
-inotifywait -qrme modify,move,create,delete /etc/openresty | while read EVENT; do
+inotifywait -qrm \
+  -e modify,move,create,delete \
+  --exclude '.*(\.pyc|~)' \
+  --exclude .swp \
+  /etc/openresty \
+  | while read EVENT; do
     echo $EVENT
     openresty -t
     if [ $? -eq 0 ]; then
